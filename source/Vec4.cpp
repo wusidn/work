0c9c4961c42584d018dfe13d7738ed15f71a5353
wusidn <wusidn@163.com>
2015-12-24 00:50:34 +0800

    Add Vec4 Struct

--- source/Vec4.cpp
+++ source/Vec4.cpp
@@ -5,7 +5,7 @@ Vec4::Vec4() {
 	Vec4(0.0, 0.0, 0.0);
 }
 
-Vec4::Vec4(const float x, const float y, const float z) {
+Vec4::Vec4(const GLfloat x, const GLfloat y, const GLfloat z) {
 
 	Vec4(x, y, z, 0.0);
 }
@@ -15,14 +15,28 @@ Vec4::Vec4(const Vec4 & source) {
 	Vec4(source.x, source.y, source.z, source.w);
 }
 
-Vec4::Vec4(const float x, const float y, const float z, const float w) {
+Vec4::Vec4(const GLfloat x, const GLfloat y, const GLfloat z, const GLfloat w) {
 	this->x = x;
 	this->y = y;
 	this->z = z;
 	this->w = w;
 }
 
-Vec4 Vec4::operator*(const float operand)
+const GLfloat Vec4::dotProduct(const Vec4 & operand)
+{
+	return this->x * operand.x + this->y * operand.y + this->z * operand.z + this->w * operand.w;
+}
+
+const Vec4 Vec4::crossProduct(const Vec4 & operand)
+{
+	return Vec4(
+			this->y * operand.z - this->z * operand.y,
+			this->z * operand.x - this->x * operand.z,
+			this->x * operand.y - this->y * operand.x
+		);
+}
+
+Vec4 Vec4::operator*(const GLfloat operand)
 {
 	return Vec4(this->x * operand,
 				this->y * operand,
@@ -30,7 +44,3 @@ Vec4 Vec4::operator*(const float operand)
 				this->w * operand);
 }
 
-float Vec4::operator*(const Vec4 & operand)
-{
-	return this->x * operand.x + this->y * operand.y + this->z * operand.z + this->w * operand.w;
-}
--- source/Vec4.h
+++ source/Vec4.h
@@ -1,24 +1,27 @@
 ﻿#pragma once
 #ifndef __ENGINE_VEC4_H__
 #define __ENGINE_VEC4_H__
+
+#include <gl\glew.h>
 #include "Object.h"
 
-class Vec4
+struct Vec4
 {
 
 public:
 
-	float x, y, z, w;
+	GLfloat x, y, z, w;
 
 	Vec4();
-	Vec4(const float x, const float y, const float z);
+	Vec4(const GLfloat x, const GLfloat y, const GLfloat z);
 	Vec4(const Vec4 & source);
 
-	Vec4(const float x, const float y, const float z, const float w);
+	Vec4(const GLfloat x, const GLfloat y, const GLfloat z, const GLfloat w);
 
-	virtual Vec4 operator*(const float operand);
-	virtual float operator*(const Vec4 & operand);
-	//virtual Vec4 operator(const Vec4 & operand);
+	const GLfloat dotProduct(const Vec4 & operand);
+	const Vec4 crossProduct(const Vec4 & operand);
+
+	virtual Vec4 operator*(const GLfloat operand);
 	
 };
 
